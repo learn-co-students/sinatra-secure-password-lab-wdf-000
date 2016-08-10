2c69a6e9d5b238166a2154004b01b22dbee674ac
ozPop <ozziepopovas@gmail.com>
2016-08-10 13:32:22 -0400

    update migration column name, update application_controller
    update migration column name from password -> password_digest
    update signup and login routes in application_controller

--- db/migrate/20160810171240_create_users.rb
+++ db/migrate/20160810171240_create_users.rb
@@ -2,7 +2,7 @@ class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |col|
       col.string :username
-      col.string :password
+      col.string :password_digest
     end
   end
 end
